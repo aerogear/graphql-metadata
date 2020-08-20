d8df2a4f2ce4d88b87fa84e907065e3221aaaf1c
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2020-08-20 16:08:02 +0200

    fix: annotation detection not working when description contains several annotations with same prefix
    Fixes https://github.com/aerogear/graphql-metadata/issues/29

--- src/annotations/stripAnnotations.ts
+++ src/annotations/stripAnnotations.ts
@@ -2,10 +2,11 @@
  * @param {string?} description
  */
 export function stripAnnotations(description: string) {
-  if (description) {
-    let lines = description.split('\n')
-    lines = lines.filter(line => !line.trim().startsWith('@'))
-    description = lines.join('\n')
+  if (!description) {
+    return description;
   }
-  return description
+
+  return description.split('\n')
+  .filter(line => !line.trim().startsWith('@'))
+  .join('\n')
 }
